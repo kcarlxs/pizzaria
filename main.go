dae832654a15c64bdae4f5e7e211e927f4c9dba6
kcarlxs <carloshenriquebragacoelho@gmail.com>
2026-05-02 23:02:22 -0300

    end of project

--- main.go
+++ main.go
@@ -1,20 +1,89 @@
 package main
 
-import "fmt"
+import (
+	"strconv"
+	"pizzaria/models"
+	"github.com/gin-gonic/gin"
+	"os"
+	"fmt"
+	"encoding/json"
 
-type Pizza struct{
-	ID int 
-	nome string
-	preco float64
-}
+)
+
+var pizzas []models.Pizza
 
 func main() {
-	var pizzas = []Pizza{
-		{ID: 1, nome: "Toscana", preco: 49.5},
-		{ID: 2, nome: "Calabresa", preco: 39.5},
-		{ID: 3, nome: "Frango com Catupiry", preco: 44.5},
-		{ID: 4, nome: "Portuguesa", preco: 42.5},
-		{ID: 5, nome: "Marguerita", preco: 35.5},
+
+	loadpizzas()
+
+	router := gin.Default()
+
+	router.GET("/pizzas", getPizzas)
+	router.POST("/pizzas", postPizzas)
+	router.GET("/pizzas/:id",getPizzasById)
+	router.Run()
+}
+
+func getPizzas(c *gin.Context) {
+
+	c.JSON(200, gin.H{
+		"pizzas": pizzas,
+	})
+}
+
+func postPizzas(c *gin.Context) {
+	var newPizza models.Pizza
+	if err := c.ShouldBindJSON(&newPizza); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
 	}
-	fmt.Println(pizzas)	
+	newPizza.Id = len(pizzas) + 1
+	pizzas = append(pizzas,newPizza)
+	savepizzas()
+	c.JSON(201, newPizza)
+}
+
+func getPizzasById(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	for _, p := range pizzas {
+		if p.Id == id {
+			c.JSON(200, p)
+			return
+		}
+	}
+	c.JSON(404, gin.H{"error": "Pizza not found"})
+}
+
+func loadpizzas() {
+	file, err := os.Open("dados/pizza.json")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&pizzas); err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+}
+
+func savepizzas() {
+	file, err := os.Create("dados/pizza.json")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	if err := encoder.Encode(pizzas); err != nil {	
+		fmt.Println("Error encoding JSON:", err)
+	}
 }
---
+++ models/pizzaria.go
@@ -0,0 +1,8 @@
+package models
+
+type Pizza struct{
+
+	Id int `json:"id"`
+	Nome string `json:"nome"`
+	Preco float64 `json:"preco"`
+}
